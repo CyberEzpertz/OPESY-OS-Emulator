82a39d7e0df61531093b5a1caf3073e7740a9c3e
sperrfeuer7 <ccccaramat@gmail.com>
2025-05-24 00:46:32 +0800

    feat: add ProcessScreen class and remove redundant const

---
+++ src/ProcessScreen.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Screen.h"
+#include "Process.h"
+#include <memory>
+#include <vector>
+#include <string>
+
+class ProcessScreen : public Screen {
+public:
+    explicit ProcessScreen(std::shared_ptr<Process> process);
+
+    void render() override;
+    void handleUserInput() override;
+
+
+private:
+    std::shared_ptr<Process> processPtr;
+};
