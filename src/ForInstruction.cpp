05b163a64c27bbd4cf4f5877a852fe7d8d3d18d8
Pricezy <parente.gabrielm@gmail.com>
2025-06-27 02:35:46 +0800

    Implemented delay on ProcessScheduler.cpp. Removed + 1 in ForInstruction.cpp

--- src/ForInstruction.cpp
+++ src/ForInstruction.cpp
@@ -16,7 +16,7 @@ ForInstruction::ForInstruction(
     }
 
     // We add 1 because the for itself is a line of code
-    lineCount = totalLineCount + 1;
+    lineCount = totalLineCount;
 }
 
 void ForInstruction::execute() {
