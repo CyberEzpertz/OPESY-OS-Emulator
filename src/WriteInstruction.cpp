91f287373c4a8e373a57597a207f964661a1eb3f
CyberEzpertz <skyblaze24@gmail.com>
2025-08-05 00:07:28 +0800

    feat: implement variable inside write instruction

--- src/WriteInstruction.cpp
+++ src/WriteInstruction.cpp
@@ -1,17 +1,27 @@
 #include "WriteInstruction.h"
 
-#include "Process.h"
 #include <format>
+
+#include "Process.h"
 
 WriteInstruction::WriteInstruction(int address, uint16_t value, int pid)
     : Instruction(1, pid), address(address), value(value) {
 }
 
+WriteInstruction::WriteInstruction(const int address, const std::string &varName, const int pid)
+    : Instruction(1, pid), address(address), value(0), varName(varName), hasVar(true) {
+}
+
 void WriteInstruction::execute() {
     const auto proc = getProcess();
+
+    if (hasVar && !varName.empty()) {
+        value = proc->getVariable(varName);
+    }
+
     proc->writeToHeap(address, value);
 }
 
 std::string WriteInstruction::serialize() const {
-    return std::format("WRITE {} {} {}", address, value, pid);
+    return std::format("WRITE {} {} {} {}", hasVar, address, hasVar ? varName : value, pid);
 }
